e7a749217d01f76cd35fd1ebd88c798aab4258b1
Vicente Matamala <vicente.matamala.v@gmail.com>
2025-04-16 00:04:41 +0000

    operational_orders

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -10,6 +10,6 @@
      ,s.logcost
      ,s.ship_cost
  FROM {{ref("int_orders_margin")}} o
- LEFT JOIN {{ref("stg_raw__ship")}} s
+ LEFT JOIN {{ref("stg_gz_raw_data__raw_gz_ship")}} s
      USING(orders_id)
  ORDER BY orders_id desc
